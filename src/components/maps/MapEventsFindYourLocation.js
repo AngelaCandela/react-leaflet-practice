ca73b956a7e5031481e1eb2a8d83e7bcba8b1fe2
Angela Candela <angelacandela@hotmail.it>
2021-09-30 17:34:13 +0200

    Create LocationMarker

--- src/components/maps/MapEventsFindYourLocation.js
+++ src/components/maps/MapEventsFindYourLocation.js
@@ -3,6 +3,11 @@ import { TileLayer, Marker, Popup } from 'react-leaflet';
 import StyledMapContainer from '../mapContainers/StyledMapContainer';
 
 const MapEventsFindYourLocation = () => {
+
+  const LocationMarker = () => {
+
+  };
+
   return(
     <StyledMapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13} scrollWheelZoom={false}>
       <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
