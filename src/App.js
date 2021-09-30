a54162cbd8e25dacedf6e0704c6814cc8c51ec25
Angela Candela <angelacandela@hotmail.it>
2021-09-30 15:58:08 +0200

    Import and render Map1 into App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Map1 from './components/maps/Map1';
 
 function App() {
   return (
     <>
       <div id="mapId"></div>
       <div id="otherMapId"></div>
+      <Map1 />
     </>
   );
 }
