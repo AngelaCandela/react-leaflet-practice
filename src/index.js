5b998390dd664d3e29bb42f433f35b36238ff123
Angela Candela <angelacandela@hotmail.it>
2021-09-30 12:11:30 +0200

    Set up the map of the center of London

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import L from 'leaflet';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYW5nZWxhY2FuZGVsYSIsImEiOiJja3U2cjllMTUzdm4zMnJxdGI4Zm10cHpiIn0.T0libTGY0nQcbu6gGP4caA'
+}).addTo(mymap);
