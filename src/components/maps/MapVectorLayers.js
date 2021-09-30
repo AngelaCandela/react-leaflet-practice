b2d20b26b3253cf660320c3e79f2340a0ebaa0b9
Angela Candela <angelacandela@hotmail.it>
2021-09-30 18:28:45 +0200

    Define set of positions to be connected by a polyline

--- src/components/maps/MapVectorLayers.js
+++ src/components/maps/MapVectorLayers.js
@@ -5,6 +5,12 @@ import StyledMapContainer from '../mapContainers/StyledMapContainer';
 const MapVectorLayers = () => {
   
   const center = [51.505, -0.09];
+
+  const polyline = [
+    [51.505, -0.09],
+    [51.51, -0.1],
+    [51.51, -0.12],
+  ];
   return(
     <StyledMapContainer center={center} zoom={13} scrollWheelZoom={false}>
       <TileLayer
