f046f294d1a60912ec51908fc7b769bb1bcd490b
Angela Candela <angelacandela@hotmail.it>
2021-09-30 17:41:24 +0200

    Add LocationMarker in MapEventsFindYourLocation

--- src/components/maps/MapEventsFindYourLocation.js
+++ src/components/maps/MapEventsFindYourLocation.js
@@ -30,6 +30,7 @@ const MapEventsFindYourLocation = () => {
       <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <LocationMarker />
     </StyledMapContainer>
   )
 };
