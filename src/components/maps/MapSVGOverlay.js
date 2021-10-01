3ed861fd031072af29ee90d3de2909f12969aa79
Angela Candela <angelacandela@hotmail.it>
2021-10-01 10:43:02 +0200

    Import and add SVGOverlay into MapSVGOverlay

--- src/components/maps/MapSVGOverlay.js
+++ src/components/maps/MapSVGOverlay.js
@@ -1,5 +1,5 @@
 import 'leaflet/dist/leaflet.css';
-import { TileLayer } from 'react-leaflet';
+import { TileLayer, SVGOverlay } from 'react-leaflet';
 import StyledMapContainer from '../mapContainers/StyledMapContainer';
 
 const MapSVGOverlay = () => {
@@ -16,6 +16,14 @@ const MapSVGOverlay = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <SVGOverlay attributes={{ stroke: 'purple' }} bounds={bounds}>
+        <rect x="0" y="0" width="100%" height="100%" fill="orange" />
+        <circle r="5" cx="10" cy="70" fill="red" />
+        <text x="20%" y="50%" stroke="black">
+          SVG Overlay
+        </text>
+      </SVGOverlay>
+    </StyledMapContainer>
   );
 };
 
