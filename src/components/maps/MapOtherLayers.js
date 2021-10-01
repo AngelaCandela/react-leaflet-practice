1620ea9c33f616a2e4138da325fbe6d684572436
Angela Candela <angelacandela@hotmail.it>
2021-10-01 11:02:49 +0200

    Define center for StyledMapContainer

--- src/components/maps/MapOtherLayers.js
+++ src/components/maps/MapOtherLayers.js
@@ -3,8 +3,11 @@ import StyledMapContainer from '../mapContainers/StyledMapContainer';
 
 const MapOtherLayers = () => {
 
+  const center = [51.505, -0.09];
+
   return (
     <StyledMapContainer>
+    <StyledMapContainer center={center} zoom={13} scrollWheelZoom={false}>
 
     </StyledMapContainer>
   );
