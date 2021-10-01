a5270e284988c549b763b08862f37ef53c97331e
Angela Candela <angelacandela@hotmail.it>
2021-10-01 10:38:35 +0200

    Create MapSVGOverlay

---
+++ src/components/maps/MapSVGOverlay.js
@@ -0,0 +1,11 @@
+import 'leaflet/dist/leaflet.css';
+import { TileLayer } from 'react-leaflet';
+import StyledMapContainer from '../mapContainers/StyledMapContainer';
+
+const MapSVGOverlay = () => {
+
+  return (
+  );
+};
+
+export default MapSVGOverlay;
