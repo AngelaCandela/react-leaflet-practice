951cb1297c9e4536ddd96d9172b74cf7bc5aa660
Angela Candela <angelacandela@hotmail.it>
2021-10-01 11:00:07 +0200

    Create MapOtherLayers

---
+++ src/components/maps/MapOtherLayers.js
@@ -0,0 +1,13 @@
+import 'leaflet/dist/leaflet.css';
+import StyledMapContainer from '../mapContainers/StyledMapContainer';
+
+const MapOtherLayers = () => {
+
+  return (
+    <StyledMapContainer>
+
+    </StyledMapContainer>
+  );
+};
+
+export default MapOtherLayers;
