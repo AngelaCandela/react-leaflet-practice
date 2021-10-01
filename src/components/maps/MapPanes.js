cb473228899a302f4d504a7e7049442bc69453fd
Angela Candela <angelacandela@hotmail.it>
2021-10-01 15:41:00 +0200

    Add BlinkingPane to Map Panes

--- src/components/maps/MapPanes.js
+++ src/components/maps/MapPanes.js
@@ -38,6 +38,7 @@ const MapPanes = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <BlinkingPane />
     </StyledMapContainer>
     </>
   )
