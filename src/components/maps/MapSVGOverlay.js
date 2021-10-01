93514b5dc17547e52c90bf1d30849eeca44f4ab6
Angela Candela <angelacandela@hotmail.it>
2021-10-01 10:40:52 +0200

    Add StyledMapContainer

--- src/components/maps/MapSVGOverlay.js
+++ src/components/maps/MapSVGOverlay.js
@@ -6,6 +6,11 @@ const MapSVGOverlay = () => {
 
   const position = [51.505, -0.09];
   return (
+    <StyledMapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
   );
 };
 
