e7aca0784e9118d6de604731d9b638ebcf620908
Angela Candela <angelacandela@hotmail.it>
2021-10-01 15:31:16 +0200

    Create Map Panes

---
+++ src/components/maps/MapPanes.js
@@ -0,0 +1,25 @@
+import 'leaflet/dist/leaflet.css';
+import { TileLayer } from 'react-leaflet';
+import StyledMapContainer from '../mapContainers/StyledMapContainer';
+
+const MapPanes = () => {
+
+  const outer = [
+    [50.505, -29.09],
+    [52.505, 29.09],
+  ];
+
+  return(
+    <>
+    <h3>Map Panes</h3>
+    <StyledMapContainer bounds={outer} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </StyledMapContainer>
+    </>
+  )
+};
+
+export default MapPanes;
